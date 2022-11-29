0aed454e105d486f8df3098667d7d3e10a8a9903
drakeum <pb26413799@live.com>
2022-11-29 17:34:55 -0500

    Renamed main to StoreTester

--- src/main/java/cop4331/Main.java
+++
@@ -1,7 +0,0 @@
-package cop4331;/**
-  * @author Hunter B.
-  */public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/cop4331/StoreTester.java
@@ -0,0 +1,12 @@
+package cop4331;
+
+/**
+ * @author Hunter B.
+ */
+public class StoreTester
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
