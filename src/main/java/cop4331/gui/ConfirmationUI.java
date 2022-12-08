3a2dab9680d57a9d7bcbd73c484e7600f38f76ef
Mark <computerpc@marks-MBP.hsd1.fl.comcast.net>
2022-12-08 11:50:28 -0500

    changes

--- src/main/java/cop4331/gui/ConfirmationUI.java
+++ src/main/java/cop4331/gui/ConfirmationUI.java
@@ -2,14 +2,27 @@
 package cop4331.gui;
 
 import javax.swing.JOptionPane;
-
+/*
+* Mark A.
+* class for confirming a slection
+*/
 public class ConfirmationUI {
     
     int confirm;
+    /*
+    * Constructor for the confirmationUI
+    * @param text - the text value shown the user
+    * @param textType - the string value corresponding to the type of confirmation
+    */
     public ConfirmationUI(String text, String textType){
         int confirm = JOptionPane.showOptionDialog(null, text, textType, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, 0);
     }
     
+    /*
+    * Mark A.
+    * returns an int value corresponding to the selected value
+    * @return integer 0 or 1 representing the selected value
+    */
     public int getSelection(){
          return confirm;
     }
